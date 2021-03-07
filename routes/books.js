166a05f088ec4706d551065745ae435c493ed932
Joham Medina <joham.smc@gmail.com>
2021-03-06 19:13:38 -0500

    The loans part is added, the user and lending data is created in the data folder, and the book lending view is created

--- routes/books.js
+++ routes/books.js
@@ -5,9 +5,11 @@ const fs = require("fs");
 const rootDir = require("../util/path");
 const router = express.Router();
 let books = require("../data/books.json");
+let users = require("../data/users.json");
+let loans = require("../data/loans.json");
 
 // / => GET
-router.get("/", (req, res, next) => {
+router.get(["/", "/index"], (req, res, next) => {
   books = fs.readFileSync(path.join(rootDir, "data", "books.json"));
   books = JSON.parse(books);
   // console.log(books);
@@ -15,15 +17,13 @@ router.get("/", (req, res, next) => {
   res.render("books", {
     pageTitle: "Lista Libros",
     books: books,
-    resultAddBook : null
+    resultAddBook: null,
   });
 });
 
 // /add-book => POST
 router.post("/add-book", (req, res, next) => {
-  const currentBook = books.find(
-    (book) => book.ISBN.toLowerCase() === req.body.ISBN.toLowerCase()
-  );
+  const currentBook = books.find((book) => book.ISBN === req.body.ISBN);
   let resultAddBook;
   if (currentBook) {
     resultAddBook = "incorrect";
@@ -44,6 +44,141 @@ router.post("/add-book", (req, res, next) => {
     pageTitle: "Lista Libros",
     books: books,
     resultAddBook: resultAddBook,
+  });
+});
+
+// /loan-book => GET
+router.get("/loan-book", (req, res, next) => {
+  books = fs.readFileSync(path.join(rootDir, "data", "books.json"));
+  books = JSON.parse(books);
+  users = fs.readFileSync(path.join(rootDir, "data", "users.json"));
+  users = JSON.parse(users);
+
+  res.render("bookLoans", {
+    pageTitle: "Lista Libros",
+    books: books,
+    users: users,
+    resultLoanBook: null,
+  });
+});
+
+// /loan-book-oldUser => POST
+router.post("/loan-book-oldUser", (req, res, next) => {
+  books = fs.readFileSync(path.join(rootDir, "data", "books.json"));
+  books = JSON.parse(books);
+  users = fs.readFileSync(path.join(rootDir, "data", "users.json"));
+  users = JSON.parse(users);
+  loans = fs.readFileSync(path.join(rootDir, "data", "loans.json"));
+  loans = JSON.parse(loans);
+
+  let resultLoanBook;
+  console.log("---------------------------------");
+  console.log(req.body);
+  console.log("---------------------------------");
+  console.log("-------------------------- Usuario Antiguo");
+
+  const currentBook = books.find((book) => book.ISBN === req.body.bookList);
+  const currentUser = users.find((user) => user.IdNumber === req.body.userList);
+  console.log(currentBook);
+  console.log(currentUser);
+  if (
+    currentBook &&
+    currentUser &&
+    currentBook.amountBorrowed + 1 <= currentBook.quantity
+  ) {
+    currentBook.amountBorrowed += 1;
+    currentUser.hasBooksLoan = true;
+
+    const dataBooks = JSON.stringify(books);
+    fs.writeFileSync(path.join(rootDir, "data", "books.json"), dataBooks);
+
+    //* Crear nuevo préstamo
+    loans.push({
+      IdNumber: req.body.userList,
+      ISBN: req.body.bookList,
+      loanDate: req.body.loanDate,
+      returnDate: req.body.returnDate,
+      return: false,
+    });
+
+    const dataLoans = JSON.stringify(loans);
+    //* Escribir en data el nuevo préstamo
+    fs.writeFileSync(path.join(rootDir, "data", "loans.json"), dataLoans);
+    resultLoanBook = "successful";
+  } else {
+    resultLoanBook = "incorrect";
+  }
+  console.log(resultLoanBook);
+  res.render("bookLoans", {
+    pageTitle: "Lista Libros",
+    books: books,
+    users: users,
+    resultLoanBook: resultLoanBook,
+  });
+});
+
+// /loan-book-newUser => POST
+router.post("/loan-book-newUser", (req, res, next) => {
+  books = fs.readFileSync(path.join(rootDir, "data", "books.json"));
+  books = JSON.parse(books);
+  users = fs.readFileSync(path.join(rootDir, "data", "users.json"));
+  users = JSON.parse(users);
+  loans = fs.readFileSync(path.join(rootDir, "data", "loans.json"));
+  loans = JSON.parse(loans);
+
+  let resultLoanBook;
+  console.log("---------------------------------");
+  console.log(req.body);
+  console.log("---------------------------------");
+  console.log("-------------------------- Usuario Nuevo");
+  const currentUser = users.find((user) => user.IdNumber === req.body.IdNumber);
+  if (!currentUser) {
+    const currentBook = books.find((book) => book.ISBN === req.body.bookList);
+    console.log(currentBook);
+    if (currentBook && currentBook.amountBorrowed + 1 <= currentBook.quantity) {
+      currentBook.amountBorrowed += 1;
+      const dataBooks = JSON.stringify(books);
+      fs.writeFileSync(path.join(rootDir, "data", "books.json"), dataBooks);
+
+      //* Crear nuevo Usuario
+      users.push({
+        IdNumber: req.body.IdNumber,
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        hasBooksLoan: true,
+      });
+      //* Escribir en data el nuevo usuario
+      const dataUsers = JSON.stringify(users);
+      fs.writeFileSync(path.join(rootDir, "data", "users.json"), dataUsers);
+
+      //* Crear nuevo préstamo
+      loans.push({
+        IdNumber: req.body.IdNumber,
+        ISBN: req.body.bookList,
+        loanDate: req.body.loanDate,
+        returnDate: req.body.returnDate,
+        return: false,
+      });
+      //* Escribir en data el nuevo préstamo
+      const dataLoans = JSON.stringify(loans);
+      fs.writeFileSync(path.join(rootDir, "data", "loans.json"), dataLoans);
+
+      resultLoanBook = "successful";
+    } else {
+      resultLoanBook = "incorrect";
+    }
+  } else {
+    resultLoanBook = "incorrect";
+  }
+
+  console.log(resultLoanBook);
+
+  res.render("bookLoans", {
+    pageTitle: "Lista Libros",
+    books: books,
+    users: users,
+    resultLoanBook: resultLoanBook,
   });
 });
 
